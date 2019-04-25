9c8aef39d3719ccafbce29ae5e7de45442933309
Brian Nguyen <thebrianbn@gmail.com>
2019-04-24 22:41:40 -0400

    both funcs

--- notebooks/ahe_hybrid.py
+++ notebooks/ahe_hybrid.py
@@ -76,6 +76,73 @@ def adaptive_hist_eq_mpi(img, slider_len, worker):
 
     return final_img.astype(int)
 
+def adaptive_hist_eq_omp(img, slider_len, worker):
+    """ Apply sliding window adaptive histogram equalization to an image
+    for improved local contrast. """
+    
+    # make a copy of original to replace pixels
+    final_img = copy(img)
+    n = len(img)
+    m = len(img[0])
+
+    gap = int(slider_len[0]// 2)  # left and right shifts
+    
+    # top worker handles top border
+    if worker=="top":
+        for i in range(gap):
+            for j in range(gap, m-gap):
+                center_pixel_val = img[i, j]
+                final_img[i, j] = window_hist(img[:i+gap,j-gap:j+gap], center_pixel_val, None)
+        for i in range(gap):
+            for j in range(gap):
+                center_pixel_val = img[i, j]
+                final_img[i, j] = window_hist(img[:i+gap,:j+gap], center_pixel_val, None)
+        for i in range(gap):
+            for j in range(m-gap, m):
+                center_pixel_val = img[i, j]
+                final_img[i, j] = window_hist(img[:i+gap,j-gap:], center_pixel_val, None)
+            
+    # bottom worker handles bottom border
+    elif worker=="bottom":
+        for i in range(n-gap, n):
+            for j in range(gap, m-gap):
+                center_pixel_val = img[i, j]
+                final_img[i, j] = window_hist(img[i-gap:n,j-gap:j+gap], center_pixel_val, None)
+        for i in range(n-gap, n):
+            for j in range(m-gap, m):
+                center_pixel_val = img[i, j]
+                final_img[i, j] = window_hist(img[i-gap:,j-gap:], center_pixel_val, None)
+        for i in range(n-gap, n):
+            for j in range(gap):
+                center_pixel_val = img[i, j]
+                final_img[i, j] = window_hist(img[i-gap:,:j+gap], center_pixel_val, None)
+
+    # every worker handles left and right borders
+    for i in range(gap, n-gap):
+        for j in range(gap):
+            center_pixel_val = img[i, j]
+            final_img[i, j] = window_hist(img[i-gap:i+gap,:j+gap], center_pixel_val, None)
+    for i in range(gap, n-gap):
+        for j in range(m-gap, m):
+            center_pixel_val = img[i, j]
+            final_img[i, j] = window_hist(img[i-gap:i+gap,j-gap:m], center_pixel_val, None)
+
+    # for each pixel in the center of the image, apply adaptive histogram equalization
+    for i in range(gap, n - gap):
+        for j in range(gap, m - gap):
+            center_pixel_val = img[i, j]
+            final_img[i, j] = window_hist(img[i-gap:i+gap, j-gap:j+gap], center_pixel_val, slider_len)
+    
+    # only return areas the worker worked on
+    if worker == "top":
+        final_img = final_img[:n-gap, :].astype(int)
+    elif worker == "bottom":
+        final_img = final_img[gap:, :].astype(int)
+    else:
+        final_img = final_img[gap:n-gap, :].astype(int)
+
+    return final_img
+
 ######## Helper Functions : End ########
 ################################################################################################
 comm = MPI.COMM_WORLD
